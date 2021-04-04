ada9a29456c85b8a1c270d525399d126ce4fabcb
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-04-04 16:46:25 +0200

    Login , logout functions

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,20 +6,41 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum;
 class LoginController extends Controller{
 
-    public function login(){
+    public function login(Request $request){
+
+        $user_inputs = $request->validate([
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string','min:8'],
+        ]);
+
+        $user = User::where('email',$user_inputs['email'])->first();
+
+        if (! $user){
+            return response([
+                'message'=>'User not exist'
+            ],401);
+        }
+        if (!Hash::check($user_inputs['password'],$user->password)){
+            return response([
+                'message'=>'Wrong credentials'
+            ],401);
+        }
+        $token = $user->createToken('jwt_token');
+        return $token;
 
 
 
     }
+
     public function logout(){
-//        return User::all()->where('id','=',1)->currentAccessToken();
-//        return User::all()->tokens();
-        return User::find(1)->tokens()->where('id',1)->delete();
-
-
+        User::find(1)->tokens()->where('tokenable_id',1)->delete();
+        return response([
+            'message'=>'logout successfully'
+        ],201);
     }
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\HasApiTokens;
-class User extends Authenticatable{
+class User extends Authenticatable {
 
     use HasApiTokens,HasFactory, Notifiable;
-
 
     protected $fillable = [
         'name',
