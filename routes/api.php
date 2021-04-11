56e304d7352f8bca856b910862230c9855a93b5c
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-04-11 20:32:26 +0200

    Create ContactController,methode:send_email(),api-route

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,20 @@
 
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\ContactEmail;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 
-
+//Authentication routes
 Route::post('/registration',[RegisterController::class,'registration']);
 Route::post('/login',[LoginController::class,'login']);
 Route::post('/password/email',[ForgotPasswordController::class,'forgot']);
 Route::get('/password.reset',function (){return view('reset_password');});
 Route::post('/password/reset',[ForgotPasswordController::class,'reset']);
 
+
+//Contact-Page routes
+Route::post('/contact/email',[ContactEmail::class,'send_email']);
 
 Route::middleware(['auth:sanctum'])->group(function(){
     Route::get('/user', function() { return auth()->user();});
