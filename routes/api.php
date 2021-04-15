732ac002da149cb29cbd789e0a3e2a567252d396
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-04-15 11:41:15 +0200

    Refactoring APi route witch ENV file

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::middleware(['auth:sanctum'])->group(function(){
 });
 
 
+
 //Contact-Page-Client : Email and Chat
 Route::post('/contact/email',[ContactEmailController::class,'send_email']);
 
@@ -30,11 +31,13 @@ Route::middleware(['auth:sanctum'])->group(function(){
     Route::post('chat/remove_chat_session',[ContactChatController::class,'remove_chat_session']);
     Route::post('chat/add_message',[ContactChatController::class,'addMessage']);
 
+
+
 });
 
 Route::post('/pusher/auth',function (Request $request){
     $socket_id = $request->request->get("socket_id");
-    $thePusher = new Pusher(env('PUSHER_APP_KEY'),env('PUSHER_APP_SECRET'),env('PUSHER_APP_ID'),array('cluster'=>'eu'));
+    $thePusher = new Pusher('8a34625906a44e573ba7','f47f12dccf48e6e0286a','1169667',array('cluster'=>'eu'));
     return response($thePusher->socket_auth('private-my-channel', $socket_id),200) ;
 });
 
