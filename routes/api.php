472ba33d232e1aafe4af2145f3732e9ded4917db
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-04-24 17:56:16 +0200

    Pusher auth replace to group auth routes

--- routes/api.php
+++ routes/api.php
@@ -21,20 +21,23 @@ Route::post('/password/reset',[ForgotPasswordController::class,'reset']);
 Route::middleware(['auth:sanctum'])->group(function(){
     Route::get('/user', function() { return auth()->user();});
     Route::post('/logout',[LoginController::class,'logout']);
+    Route::post('/pusher/auth',[PusherAuthController::class,'pusher_authentication']);
 });
-Route::post('/pusher/auth',[PusherAuthController::class,'pusher_authentication']);
+
+
 
 
 //Contact-Page-Client : Email and Chat
 Route::post('/contact/email',[ContactEmailController::class,'send_email']);
-
 Route::middleware(['auth:sanctum'])->group(function(){
+
     Route::get('chat/create_new_chat_session',[ContactChatController::class,'create_new_chat_session']);
     Route::get('chat/get_chat_session_messages',[ContactChatController::class,'get_chat_session_messages']);
     Route::post('chat/remove_chat_session',[ContactChatController::class,'remove_chat_session']);
     Route::post('chat/add_message',[ContactChatController::class,'addMessage']);
-
 });
+
+
 
 Route::middleware(['auth:sanctum','admin'])->group(function (){
     Route::get('admin/contact/chat/chat_session_waiting_list',[ChatController::class,'get_chat_session_waiting_list']);
